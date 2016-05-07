b0ed2ce1b6c60cd6a042e61d225191fca953b77b
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-05-07 03:22:48 +0100

    Add tests for ClassInstanceMap

---
+++ src/test/java/me/jackwilsdon/secsy/ClassInstanceMapTest.java
@@ -0,0 +1,30 @@
+package me.jackwilsdon.secsy;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClassInstanceMapTest {
+
+    @Test
+    public void testGetKey() {
+        final ClassInstanceMap<Object> instanceMap = new ClassInstanceMap<>(new HashMap<Class<?>, Object>());
+
+        assertEquals(Object.class, instanceMap.getKey(new Object()));
+    }
+
+    @Test
+    public void testGetCast() {
+        final ClassInstanceMap<Object> instanceMap = new ClassInstanceMap<>(new HashMap<Class<?>, Object>());
+
+        final Object value = new Object();
+
+        instanceMap.put(null, value);
+
+        final Object retrievedValue = instanceMap.getCast(Object.class);
+
+        assertEquals(value, retrievedValue);
+    }
+}
