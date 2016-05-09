d4a125942919391545dff5907cc5f5935b43a1b7
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-05-10 00:02:09 +0100

    Make ClassInstanceMapTest final

--- src/test/java/me/jackwilsdon/secsy/ClassInstanceMapTest.java
+++ src/test/java/me/jackwilsdon/secsy/ClassInstanceMapTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class ClassInstanceMapTest {
+public final class ClassInstanceMapTest {
 
     @Test
     public void testGetKey() {
