e5220a15a6fa7ac28b1c261efb55e35a9e67eed4
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-06-25 12:57:07 -0400

    Improve ClassInstanceMap tests
     - Add JavaDoc comments to tests describing their functionality.
     - Remove pointless test for getKey (it's just a getter really).
     - Use an Integer object instead of a plain object when testing casting.

--- src/test/java/me/jackwilsdon/secsy/ClassInstanceMapTest.java
+++ src/test/java/me/jackwilsdon/secsy/ClassInstanceMapTest.java
@@ -6,26 +6,38 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests for {@link ClassInstanceMap}.
+ *
+ * @see ClassInstanceMap
+ */
 public final class ClassInstanceMapTest {
 
+    /**
+     * Tests for {@link ClassInstanceMap}.
+     *
+     * @see ClassInstanceMap
+     */
     public ClassInstanceMapTest() { }
 
-    @Test
-    public void testGetKey() {
-        final ClassInstanceMap<Object> instanceMap = new ClassInstanceMap<>(new HashMap<Class<?>, Object>());
-
-        assertEquals(Object.class, instanceMap.getKey(new Object()));
-    }
-
+    /**
+     * Tests getting a cast value from the map.
+     *
+     * <p>
+     *     This tests puts a value in the map and then retrieves it by class using
+     *     {@link ClassInstanceMap#getCast(Class)}. It then checks the returned value against the original one.
+     * </p>
+     */
     @Test
     public void testGetCast() {
         final ClassInstanceMap<Object> instanceMap = new ClassInstanceMap<>(new HashMap<Class<?>, Object>());
 
-        final Object value = new Object();
+        final Object value = new Integer(0);
 
         instanceMap.put(null, value);
 
-        final Object retrievedValue = instanceMap.getCast(Object.class);
+        // Retrieve the value as an Integer object.
+        final Integer retrievedValue = instanceMap.getCast(Integer.class);
 
         assertEquals(value, retrievedValue);
     }
