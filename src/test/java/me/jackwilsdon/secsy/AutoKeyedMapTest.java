a43bbe2c9b3af299529cffb22e39ba793cafb08f
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-06-27 00:25:52 -0400

    Make documentation consistent in AutoKeyedMap tests

--- src/test/java/me/jackwilsdon/secsy/AutoKeyedMapTest.java
+++ src/test/java/me/jackwilsdon/secsy/AutoKeyedMapTest.java
@@ -27,8 +27,8 @@ public final class AutoKeyedMapTest {
      * Tests putting a value into the map.
      *
      * <p>
-     *     This test puts a value in the map with a {@code null} key. It then checks to ensure that the key
-     *     returned by {@link AutoKeyedMap#getKey(Object)} is correct for the provided value.
+     * This test puts a value in the map with a {@code null} key. It then checks to ensure that the key returned by
+     * {@link AutoKeyedMap#getKey(Object)} is correct for the provided value.
      * </p>
      *
      * @see AutoKeyedMap#put(Object, Object)
@@ -52,9 +52,9 @@ public final class AutoKeyedMapTest {
      * Tests putting a value into the map with {@code preventOverwrite} set to {@code true}.
      *
      * <p>
-     *     This test first creates a map with {@code preventOverwrite} set to {@code true}. It then puts a value in the
-     *     map with a {@code null} key and checks to ensure that the key returned by
-     *     {@link AutoKeyedMap#getKey(Object)} is correct for the provided value.
+     * This test first creates a map with {@code preventOverwrite} set to {@code true}. It then puts a value in the
+     * map with a {@code null} key and checks to ensure that the key returned by
+     * {@link AutoKeyedMap#getKey(Object)} is correct for the provided value.
      * </p>
      *
      * @see AutoKeyedMap#put(Object, Object)
@@ -78,10 +78,10 @@ public final class AutoKeyedMapTest {
      * Tests putting an existing value into the map.
      *
      * <p>
-     *     This test puts a value in the map with a {@code null} key twice. It stores the return value from
-     *     {@link AutoKeyedMap#put(Object, Object)} on the second call and compares the value to the first value
-     *     added to the map. This is done to ensure that {@link AutoKeyedMap#put(Object, Object)} returns the
-     *     previous value for the generated key.
+     * This test puts a value in the map with a {@code null} key twice. It stores the return value from
+     * {@link AutoKeyedMap#put(Object, Object)} on the second call and compares the value to the first value added
+     * to the map. This is done to ensure that {@link AutoKeyedMap#put(Object, Object)} returns the previous value
+     * for the generated key.
      * </p>
      *
      * @see AutoKeyedMap#put(Object, Object)
@@ -99,7 +99,7 @@ public final class AutoKeyedMapTest {
         // Put the second object in with the same automatically generated key (i.e. same hashCode value).
         final Object originalValue = autoKeyedMap.put(null, secondObject);
 
-        // Ensure that put has returned the first object (i.e. the original value for the automatically generated key).
+        // Ensure that put has returned the first object (i.e. the original value for the generated key).
         assertTrue(originalValue.equals(firstObject));
 
         // Get the generated key for the second object.
@@ -113,9 +113,9 @@ public final class AutoKeyedMapTest {
      * Tests putting an existing value into the map with {@code preventOverwrite} set to {@code true}.
      *
      * <p>
-     *     This test first creates a map with {@code preventOverwrite} set to {@code true}. It then puts a value in the
-     *     map with a {@code null} key twice. This should throw an {@link IllegalStateException}, as
-     *     {@code preventOverwrite} ensures that a key cannot be overwritten.
+     * This test first creates a map with {@code preventOverwrite} set to {@code true}. It then puts a value in the
+     * map with a {@code null} key twice. This should throw an {@link IllegalStateException}, as
+     * {@code preventOverwrite} ensures that a key cannot be overwritten.
      * </p>
      *
      * @see AutoKeyedMap#put(Object, Object)
@@ -132,9 +132,8 @@ public final class AutoKeyedMapTest {
      * Tests putting a value into the map with a non-null key.
      *
      * <p>
-     *     This test puts a value into the map with a non-null key. It should throw an
-     *     {@link UnsupportedOperationException}, as the key is automatically generated in an
-     *     {@link AutoKeyedMap}.
+     * This test puts a value into the map with a non-null key. It should throw an
+     * {@link UnsupportedOperationException}, as the key is automatically generated in an {@link AutoKeyedMap}.
      * </p>
      *
      * @see AutoKeyedMap#put(Object, Object)
@@ -170,7 +169,8 @@ public final class AutoKeyedMapTest {
      * A class that has a fake hash code (i.e. one that can be set at construction time).
      *
      * <p>
-     *     Note that this class breaks the {@code equals}/{@code hashCode} contract and should only be used for testing.
+     * Note that this class breaks the {@code equals}/{@code hashCode} contract and should only be used for
+     * testing.
      * </p>
      */
     private static final class FakeHashCodeObject {
